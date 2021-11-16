7bae065c2988793a1fd81fc981db00cc5ba8a6d9
Tammarra McPhaul <shaemc22@gmail.com>
2021-11-15 20:11:12 -0500

    commit tree f06ee76eaeefe089eab73a636d3e52ea1a87375e

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(string) {
+  let time = parseInt(string);
+  if(time < 12) {
+    return "Good Morning"
+  }
+  if (time > 12 && time < 17) {
+    return "Good Afternoon"
+  }
+  if (time > 17) {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  let message = document.getElementById('greeting');
+  message.textContent = string;
+}
